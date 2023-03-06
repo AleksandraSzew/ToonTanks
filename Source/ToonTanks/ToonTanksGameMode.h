3aa81742e9c3fac4d16e8a861e092ba81b531e4f
Aleksandra Szewczuk <aleksandraszew25@gmail.com>
2023-03-06 23:52:28 +0100

    Creating start game widget

--- Source/ToonTanks/ToonTanksGameMode.h
+++ Source/ToonTanks/ToonTanksGameMode.h
@@ -18,6 +18,8 @@ public:
 	void ActorDied(AActor* DeadActor);
 protected:
 	virtual void BeginPlay() override;
+	UFUNCTION(BlueprintImplementableEvent)
+	void StartGame();
 private:
 	class ATank* Tank;
 	class AToonTanksPlayerController* ToonTanksPlayerController;
